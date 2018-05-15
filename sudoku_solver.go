e81742f8e839b838bceda1807b6b04df3c2dc54c
zurustar <zurustar@users.noreply.github.com>
2018-05-15 22:50:59 +0900

    updated

--- sudoku_solver.go
+++ sudoku_solver.go
@@ -16,25 +16,13 @@ import (
 	"time"
 )
 
-type BoardState int
-
-const (
-	SOLVED BoardState = iota
-	NOT_SOLVED
-	INVALID
-)
-
-type UpdateResult int
-
-const (
-	UPDATED UpdateResult = iota
-	NOT_UPDATED
-	ERROR
-)
-
 var peers = [][]int{}
 
-func initPeer() {
+func Pos2Block(x, y int) int {
+	return int(math.Trunc(float64(y)/3.0)*3 + math.Trunc(float64(x)/3.0))
+}
+
+func InitPeer() {
 	for pos := 0; pos < 81; pos++ {
 		peers = append(peers, []int{})
 	}
@@ -51,12 +39,8 @@ func initPeer() {
 						peers[sp] = append(peers[sp], dp)
 					} else if sy == dy {
 						peers[sp] = append(peers[sp], dp)
-					} else {
-						sb := math.Trunc(float64(sy)/3.0)*3 + math.Trunc(float64(sx)/3.0)
-						db := math.Trunc(float64(dy)/3.0)*3 + math.Trunc(float64(dx)/3.0)
-						if sb == db {
-							peers[sp] = append(peers[sp], dp)
-						}
+					} else if Pos2Block(sx, sy) == Pos2Block(dx, dy) {
+						peers[sp] = append(peers[sp], dp)
 					}
 				}
 			}
@@ -64,16 +48,12 @@ func initPeer() {
 	}
 }
 
-type Board struct {
-	cells [9 * 9][]int
-}
+type Board [9 * 9][]int
 
 func NewBoard() *Board {
 	b := new(Board)
-	for row := 0; row < 9; row++ {
-		for column := 0; column < 9; column++ {
-			b.cells[row*9+column] = []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
-		}
+	for p := 0; p < 81; p++ {
+		b[p] = []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
 	}
 	return b
 }
@@ -81,8 +61,8 @@ func NewBoard() *Board {
 func (board *Board) ToString() string {
 	s := ""
 	for i := 0; i < 9*9; i++ {
-		if len(board.cells[i]) == 1 {
-			s += strconv.Itoa(board.cells[i][0])
+		if len(board[i]) == 1 {
+			s += strconv.Itoa(board[i][0])
 		} else {
 			s += "0"
 		}
@@ -92,76 +72,40 @@ func (board *Board) ToString() string {
 
 func (board *Board) ShowDetail() {
 	s := ""
+	for i := 0; i < 90; i++ {
+		s += "="
+	}
+	s += "\n"
 	for y := 0; y < 9; y++ {
+		s += "|"
 		for x := 0; x < 9; x++ {
 			p := y*9 + x
-			s += " "
-			for v := 1; v < 4; v++ {
+			for v := 1; v < 10; v++ {
 				if board.Find(p, v) >= 0 {
 					s += strconv.Itoa(v)
 				} else {
 					s += " "
 				}
 			}
-			if x == 2 || x == 5 {
-				s += "|"
-			} else if x != 8 {
-				s += ":"
-			}
-		}
-		s += "\n"
-		for x := 0; x < 9; x++ {
-			p := y*9 + x
-			s += " "
-			for v := 4; v < 7; v++ {
-				if board.Find(p, v) >= 0 {
-					s += strconv.Itoa(v)
-				} else {
-					s += " "
-				}
-			}
-			if x == 2 || x == 5 {
-				s += "|"
-			} else if x != 8 {
-				s += ":"
-			}
-		}
-		s += "\n"
-		for x := 0; x < 9; x++ {
-			p := y*9 + x
-			s += " "
-			for v := 7; v < 10; v++ {
-				if board.Find(p, v) >= 0 {
-					s += strconv.Itoa(v)
-				} else {
-					s += " "
-				}
-			}
-			if x == 2 || x == 5 {
-				s += "|"
-			} else if x != 8 {
-				s += ":"
-			}
+			s += "|"
 		}
 		s += "\n"
 		if y == 2 || y == 5 {
-			s += " ==========================================="
-		} else if y != 8 {
-			s += " -------------+--------------+--------------"
+			for i := 0; i < 90; i++ {
+				s += "-"
+			}
+			s += "\n"
 		}
-		s += "\n"
 	}
+	for i := 0; i < 90; i++ {
+		s += "="
+	}
+	s += "\n"
 	fmt.Println(s)
 }
 
-func (board *Board) CopyFrom(src *Board) {
-	for pos := 0; pos < 9*9; pos++ {
-		board.cells[pos] = src.cells[pos]
-	}
-}
-
 func (board *Board) Find(pos, value int) int {
-	for i, v := range board.cells[pos] {
+	for i, v := range board[pos] {
 		if v == value {
 			return i
 		}
@@ -172,10 +116,10 @@ func (board *Board) Find(pos, value int) int {
 //
 // 指定された場所のセルの候補リストから指定された値を削除する
 //
-func (board *Board) Remove(pos, value int) UpdateResult {
+func (board *Board) Remove(pos, value int) int {
 	found := false
 	cands := []int{}
-	for _, v := range board.cells[pos] {
+	for _, v := range board[pos] {
 		if v == value {
 			found = true
 		} else {
@@ -184,127 +128,107 @@ func (board *Board) Remove(pos, value int) UpdateResult {
 	}
 	if found {
 		if len(cands) == 0 {
-			return ERROR // 消したらデータがなくなってしまった
+			return -1
 		}
-		board.cells[pos] = cands
-		return UPDATED // 消した
+		board[pos] = cands
+		return 1
 	}
-	return NOT_UPDATED // 該当のデータがなかったので消さなかった
-}
-
-//
-// 各セルに着目し、候補数があとひとつに減っていたら、そのセルはその値で
-// 確定ということになるので、横方向、縦方向、同じグループ内の他のセルの
-// 候補からその値を削除する
-//
-func (board *Board) _Update1() UpdateResult {
-	updated := false
-	for sp := 0; sp < 81; sp++ {
-		if len(board.cells[sp]) != 1 {
-			continue
-		}
-		for _, dp := range peers[sp] {
-			switch board.Remove(dp, board.cells[sp][0]) {
-			case UPDATED:
-				updated = true
-			case NOT_UPDATED:
-				// do nothing
-			case ERROR:
-				return ERROR
-			}
-		}
-	}
-	if updated {
-		return UPDATED
-	}
-	return NOT_UPDATED
-}
-
-//
-// 各セルの各候補に着目し、その値が横方向、縦方向、同じグループ内の他の
-// セルの候補にその値が含まれていなかったら、その値に確定
-//
-func (board *Board) _Update2() bool {
-	updated := false
-	for sp := 0; sp < 81; sp++ {
-		if len(board.cells[sp]) == 1 {
-			continue
-		}
-		for _, v := range board.cells[sp] {
-			found := false
-			for _, dp := range peers[sp] {
-				if board.Find(dp, v) >= 0 {
-					found = true
-					break
-				}
-			}
-			if found == false {
-				board.cells[sp] = []int{v}
-				updated = true
-			}
-		}
-	}
-	return updated
+	return 0
 }
 
 //
 // 各セルの候補を絞り込む
 //
-func (board *Board) Update() UpdateResult {
-	updated := true
-	for updated {
-		switch board._Update1() {
-		case UPDATED:
-			updated = true
-		case NOT_UPDATED:
-			updated = board._Update2()
-		case ERROR:
-			return ERROR
-		}
-	}
-	return UPDATED
-}
-
-//
-// 解く。ルールどおりに試して答えがでなかったら仮置きして再帰呼び出し
-//
-func Solve(board *Board) (BoardState, *Board) {
-	if board.Update() == UPDATED {
-		// 各セルそれぞれにのこりいくつの候補が残っているかを調査
-		// 残りの個数ごとにそのセルの番号を配列に格納する
-		len_list := [10][]int{{}, {}, {}, {}, {}, {}, {}, {}, {}, {}}
-		for pos := 0; pos < 9*9; pos++ {
-			l := len(board.cells[pos])
-			len_list[l] = append(len_list[l], pos)
-		}
-		// 候補数がゼロのセルが存在したら異常な状態になっている
-		if len(len_list[0]) > 0 {
-			return INVALID, nil
-		}
-		// すべてのセルの候補数が1になっていたら解けている
-		if len(len_list[1]) == 9*9 {
-			return SOLVED, board
-		}
-		// まだ候補数が１になっていないセルについて、
-		// 適当にいっこの値に絞り込んで、ためしに解いてみる
-		// 再帰呼び出しになるので、結果的にかたっぱしから試すことになる
-		for i := 2; i < 10; i++ {
-			for _, pos := range len_list[i] {
-				if len(board.cells[pos]) > 1 {
-					for _, cand := range board.cells[pos] {
-						new_board := NewBoard()
-						new_board.CopyFrom(board)
-						new_board.cells[pos] = []int{cand}
-						result, new_board := Solve(new_board)
-						if result == SOLVED {
-							return SOLVED, new_board
+func (board *Board) Update() int {
+	flag, updated := true, false
+	for flag {
+		flag = false
+		for sp := 0; sp < 81; sp++ {
+			if len(board[sp]) == 1 {
+				// すでに候補がひとつしかなかったら
+				// お友達からその候補を削除する
+				for _, dp := range peers[sp] {
+					switch board.Remove(dp, board[sp][0]) {
+					case 1:
+						flag, updated = true, true
+					case -1:
+						return -1
+					}
+				}
+			} else {
+				// 候補がたくさん残っていたら、それぞれの候補について
+				// お友達が持っていなかったらその候補に確定
+				for _, v := range board[sp] {
+					found := false
+					for _, dp := range peers[sp] {
+						if board.Find(dp, v) >= 0 {
+							found = true
+							break
 						}
+					}
+					if found == false {
+						board[sp] = []int{v}
+						flag, updated = true, true
 					}
 				}
 			}
 		}
 	}
-	return NOT_SOLVED, board
+	if updated {
+		return 1
+	}
+	return 0
+}
+
+//
+// 解く。ルールどおりに試して答えがでなかったら仮置きして再帰呼び出し
+//
+func Solve(board *Board) (int, *Board) {
+	switch board.Update() {
+	case 0:
+		return 0, board
+	case -1:
+		return -1, nil
+	}
+	// 各セルそれぞれにのこりいくつの候補が残っているかを調査
+	// 残りの個数ごとにそのセルの番号を配列に格納する
+	len_list := [10][]int{{}, {}, {}, {}, {}, {}, {}, {}, {}, {}}
+	for pos := 0; pos < 9*9; pos++ {
+		l := len(board[pos])
+		len_list[l] = append(len_list[l], pos)
+	}
+	// 候補数がゼロのセルが存在したら異常な状態になっている
+	if len(len_list[0]) > 0 {
+		return -1, nil
+	}
+	// すべてのセルの候補数が1になっていたら解けている
+	if len(len_list[1]) == 9*9 {
+		return 1, board
+	}
+	// まだ候補数が１になっていないセルについて、
+	// 適当にいっこの値に絞り込んで、ためしに解いてみる
+	// 再帰呼び出しになるので、結果的にかたっぱしから試すことになる
+	for i := 2; i < 10; i++ {
+		for _, pos := range len_list[i] {
+			if len(board[pos]) > 1 {
+				for _, cand := range board[pos] {
+					new_board := new(Board)
+					for j := 0; j < 81; j++ {
+						new_board[j] = []int{}
+						for _, v := range board[j] {
+							new_board[j] = append(new_board[j], v)
+						}
+					}
+					new_board[pos] = []int{cand}
+					result, new_board := Solve(new_board)
+					if result == 1 {
+						return 1, new_board
+					}
+				}
+			}
+		}
+	}
+	return 0, board
 }
 
 //
@@ -345,11 +269,11 @@ func RunSolver(src string) string {
 			return "" // bug?
 		}
 		if v != 0 {
-			board.cells[i] = []int{v}
+			board[i] = []int{v}
 		}
 	}
 	result, board := Solve(board)
-	if result == SOLVED {
+	if result == 1 {
 		return board.ToString()
 	}
 	return ""
@@ -454,7 +378,7 @@ func RunTwitterBot(config_filename string) {
 
 func main() {
 	if len(os.Args) == 3 {
-		initPeer()
+		InitPeer()
 		switch os.Args[1] {
 		case "-d":
 			RunTwitterBot(os.Args[2])
